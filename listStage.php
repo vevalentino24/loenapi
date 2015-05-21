656fb62fc9c3b224d37b27d74aa5088707671a61
vevalentino24 <ve.febrian@gmail.com>
2015-05-21 11:40:11 +0700

    tolong beneri API listStage.php geng

--- listStage.php
+++ listStage.php
@@ -12,6 +12,7 @@
 	$qry 				= mysql_query("SELECT * FROM table_stage");
 	$count 				= mysql_num_rows($qry);
 
+	
 	if($count!=0){
 		
 		$returnData['response']=$response;
@@ -19,9 +20,26 @@
 		$returnData['message']=$message;
 
 		while ($data = mysql_fetch_object($qry)) {
+
 			$result['id']		=$data->id;
 			$result['name']		=$data->name;
-			
+			$isi = array();
+
+			$qry_scedule 		= mysql_query("SELECT s.id, s.urutan, s.name, s.time, s.stage, i.file
+									FROM table_schedule s, table_icon i
+									WHERE s.icon = i.id 
+									AND s.stage = '".$data->id."'");
+			unset($result['isi']);
+			while ($schedule = mysql_fetch_object($qry_scedule)) {
+				$isi['id']		=$schedule->id;
+				$isi['urutan']	=$schedule->urutan;
+				$isi['name']	=$schedule->name;
+				$isi['time']	=$schedule->time;
+				$isi['stage']	=$schedule->stage;
+				$isi['icon']	=$schedule->file;
+				$result['items'][]	=$isi;
+			}
+
 			$returnData['result'][]=$result;
 		}	
 	}else{
